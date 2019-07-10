a4cae829a20ea5a387dacc76688eafa92a7d8eb9
Spencer Rose <spencerkrose@gmail.com>
2019-07-10 18:51:40 -0500

    git upload

---
+++ app/data/friends.js
@@ -0,0 +1,114 @@
+[
+    {
+        "name": "Thelonius",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Pinnochio",
+        "photo": "https://via.placeholder.com/200.jpg",
+        "scores": [
+            3,
+            1,
+            3,
+            4,
+            5,
+            4,
+            2,
+            3,
+            4,
+            2
+        ]
+    },
+    {
+        "name": "Shrek",
+        "photo": "https://via.placeholder.com/200.jpg",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "Donkey",
+        "photo": "https://via.placeholder.com/200.jpg",
+        "scores": [
+            3,
+            2,
+            3,
+            5,
+            5,
+            1,
+            3,
+            3,
+            2,
+            2
+        ]
+    },
+    {
+        "name": "Fiona",
+        "photo": "https://via.placeholder.com/200.jpg",
+        "scores": [
+            1,
+            2,
+            3,
+            5,
+            5,
+            4,
+            3,
+            2,
+            1,
+            5
+        ]
+    },
+    {
+        "name": "Dragon",
+        "photo": "https://via.placeholder.com/200.jpg",
+        "scores": [
+            4,
+            3,
+            2,
+            1,
+            3,
+            2,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Farquaad",
+        "photo": "https://via.placeholder.com/200.jpg",
+        "scores": [
+            3,
+            1,
+            3,
+            4,
+            5,
+            4,
+            2,
+            3,
+            4,
+            2
+        ]
+    }
+]
---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8060;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../FriendFinder/public/home.html"));
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../FriendFinder/public/survey.html"));
+  });
+
+app.listen(PORT, function() {
+  console.log("App listening on http://localhost:" +PORT);
+});
